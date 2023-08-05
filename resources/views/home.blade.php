3fca7478b41925d980dc3d4bc569e0d2d15c602e
Michael Isip <isipmichaeljohn@gmail.com>
2023-08-05 13:09:52 +0800

    ckeditor5 setup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card my-2">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
@@ -17,7 +17,25 @@
                     {{ __('You are logged in!') }}
                 </div>
             </div>
+            <div class="card my-2">
+                <div class="card-header">{{ __('Compose your message') }}</div>
+
+                <div class="card-body">
+                    <textarea name="message" id="ckeditor" cols="30" rows="10"></textarea>
+                </div>
+            </div>
         </div>
     </div>
 </div>
+@endsection
+
+@section('scripts')
+    <script src="https://cdn.ckeditor.com/ckeditor5/39.0.0/classic/ckeditor.js"></script>
+    <script>
+        ClassicEditor
+            .create(document.querySelector('#ckeditor'))
+            .catch(error => {
+                console.error( error );
+            });
+    </script>
 @endsection
